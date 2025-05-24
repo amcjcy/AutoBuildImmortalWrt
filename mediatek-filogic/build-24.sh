548665a2df121fdf4dfc30413937d35f19140071
amcjcy <82363754+amcjcy@users.noreply.github.com>
2025-05-24 16:47:58 +0800

    Update build-24.sh

--- mediatek-filogic/build-24.sh
+++ mediatek-filogic/build-24.sh
@@ -28,10 +28,11 @@ PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
 # PACKAGES="$PACKAGES luci-app-argon-config"
 # PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 # PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
-#23.05
+#24.10
 # PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 # PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 # PACKAGES="$PACKAGES luci-app-openclash"
+PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
 PACKAGES="$PACKAGES luci-i18n-nps-zh-cn"
